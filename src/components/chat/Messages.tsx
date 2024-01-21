f17be1040306dc19943fe27b1de3efc827d23f4d
arpesh28 <arpeshgadekar@gmail.com>
2024-01-21 23:38:03 +0530

    UI: Display messages of user & ai bot

--- src/components/chat/Messages.tsx
+++ src/components/chat/Messages.tsx
@@ -1,6 +1,8 @@
 import { trpc } from "@/app/_trpc/client";
 import { INFINITE_QUERY_LIMIT } from "@/config/infinite-query";
-import { Loader2 } from "lucide-react";
+import { Loader2, MessageSquare } from "lucide-react";
+import { Skeleton } from "../ui/skeleton";
+import Message from "./Message";
 
 interface MessagesProps {
   fileId: string;
@@ -41,16 +43,43 @@ const Messages = ({ fileId }: MessagesProps) => {
     <div className="flex max-h-[calc(100vh-3.5rem-7rem)] border-zinc-200 flex-1 flex-col-reverse gap-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch">
       {combinedMessages?.length > 0 ? (
         combinedMessages.map((message, i) => {
+          const isNextMessageSamePerson =
+            combinedMessages[i - 1]?.isUserMessage ===
+            combinedMessages[i]?.isUserMessage;
+
           if (i === combinedMessages.length - 1) {
-            return <Message />;
+            return (
+              <Message
+                key={message.id}
+                message={message}
+                isNextMessageSamePerson={isNextMessageSamePerson}
+              />
+            );
           } else {
-            return <Message />;
+            return (
+              <Message
+                key={message.id}
+                message={message}
+                isNextMessageSamePerson={isNextMessageSamePerson}
+              />
+            );
           }
         })
       ) : isLoading ? (
-        <div></div>
+        <div className="w-full flex flex-col gap-2">
+          <Skeleton className="h-16" />
+          <Skeleton className="h-16" />
+          <Skeleton className="h-16" />
+          <Skeleton className="h-16" />
+        </div>
       ) : (
-        <div></div>
+        <div className="flex-1 flex flex-col items-center justify-center gap-2">
+          <MessageSquare className="h-8 w-8 text-blue-500" />
+          <h3 className="font-semibold text-xl">You&apos;re all set!</h3>
+          <p className="text-zinc-500 text-sm">
+            Ask your first question to get started.
+          </p>
+        </div>
       )}
     </div>
   );
