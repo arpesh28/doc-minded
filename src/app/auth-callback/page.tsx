ade3db6db6a75066a3044fcd45a00fd8baceda57
arpesh28 <arpeshgadekar@gmail.com>
2024-01-31 19:17:47 +0530

    navbar changes

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -15,7 +15,7 @@ const Page = () => {
     router.push(origin ? `/${origin}` : "/dashboard");
   } else {
     if (error?.data?.code === "UNAUTHORIZED") {
-      router.push("/sign-in");
+      router.push("/");
     }
   }
 
